4a2852f4ebba9abb0f2c6e4a993e64740303a2df
MV <danilpanin2002@yandex.ru>
2021-11-15 23:14:59 +0500

    Добавьте файлы проекта.

---
+++ Test3/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test3
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<User> Operators { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=DBRimeraDefect;Username=postgres;Password=scisci");
+        }
+    }
+}
---
+++ Test3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // добавление данных
+            //using (ApplicationContext db = new ApplicationContext())
+            //{
+            //    // создаем два объекта User
+            //    User user1 = new User { Name = "Tom", Surname = "Tom2", Patronymic = "Tom3" };
+            //    User user2 = new User { Name = "Alice", Surname = "Alice2", Patronymic = "Alice3" };
+
+            //    // добавляем их в бд
+            //    db.Operators.AddRange(user1, user2);
+            //    db.SaveChanges();
+            //}
+            // получение данных
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Operators.ToList();
+                Console.WriteLine("Users list:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.id}.{u.Name} - {u.Surname}");
+                }
+            }
+        }
+    }
+}
+//Scaffold-DbContext "Host=localhost;Port=5432;Database=DBRimeraDefect;Username=postgres;Password=scisci"
+//Npgsql.EntityFrameworkCore.PostgreSQL
